6ea986bcb383b984090a4457e61ddd14abb8cd8e
yerimi00 <fhddl1019@naver.com>
2026-09-19 23:13:17 +0900

    fix : 건강/위치/카카오맵 제거

--- modules/kakao-map/src/KakaoMapModule.web.ts
+++
@@ -1,6 +0,0 @@
-import { registerWebModule, NativeModule } from "expo";
-
-// KakaoMapModule is not available on the web platform.
-class KakaoMapModule extends NativeModule<{}> {}
-
-export default registerWebModule(KakaoMapModule, "KakaoMapModule");
